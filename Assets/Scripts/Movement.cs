a5d0c366dd64cc4c6451727f64a181f2ab27911e
Laura Marchetti <s294767@studenti.polito.it>
2023-04-20 21:35:33 +0200

    double jump and code refactoring

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,40 +4,46 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    float moveSpeed;
+     //values to set in the inspector
+    public Rigidbody RB;
+    public float moveSpeed;
     public float walkSpeed;
     public float dashSpeed;
     public Transform orientation;
     public Transform playerRot;
-    Vector3 myNormal;
-
-    float horizontalInput;
-    float verticalInput;
-
     public float groundDrag;
-
     public float jumpForce;
+    public float maxSlopeAngle;
     public float jumpCooldown;
+    public int doubleJumpOffset;
     public float airMultiplier;
-    bool readyToJump;
-
     public Transform groundCheck;
     public Material PlayerMaterial;
     public LayerMask Ground;
     public LayerMask Magnetic;
-    bool isGrounded;
+    
+    //input and utilities movement variables
+    float horizontalInput;
+    float verticalInput;
+    private Vector3 myNormal;
+    private Vector3 moveDir;
+    
+    //bools for checking
+    private bool readyToJump;
+    public bool isGrounded;
     public bool isMagnetic;
+    public bool canDoubleJump=false;
+    private bool exitingSlope; //when jumping on the slop, used not remove speed limitation while jumping on the slope 
+    public bool dashing;
 
-    public float maxSlopeAngle;
+
+     //raycasts
     public RaycastHit slopeHit;
     RaycastHit magneticHit;
-    private bool exitingSlope;
+    
 
-    Vector3 moveDir;
-    Rigidbody RB;
-
+    //movement states
     public MovementState state;
-
     public enum MovementState
     {
         walking,
@@ -46,9 +52,9 @@ public class Movement : MonoBehaviour
         magnetic
     }
 
-    public bool dashing;
+   
 
-    // Start is called before the first frame update
+    
     void Start()
     {
         RB = GetComponent<Rigidbody>();
@@ -58,21 +64,30 @@ public class Movement : MonoBehaviour
         myNormal = playerRot.up;
     }
     
+
     private void MyInput()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
-        if (Input.GetKey(KeyCode.Space) && readyToJump && isGrounded)
+
+        if (Input.GetKeyDown(KeyCode.Space) && readyToJump && isGrounded)
         {
             readyToJump = false;
             Jump();
+            canDoubleJump=true;
             Invoke(nameof(ResetJump), jumpCooldown);
+
+        }else if ( Input.GetKeyDown(KeyCode.Space) && canDoubleJump==true &&  RB.velocity.y>0.1f && !isGrounded){ //doppio salto
+                RB.velocity = new Vector3(RB.velocity.x, RB.velocity.y*doubleJumpOffset, RB.velocity.z);
+                canDoubleJump=false;
         }
     }
 
-    // Update is called once per frame
+
+    
     void Update()
     {
+        //check between child object "groundCheck" & ground/magnetic layer
         isGrounded = Physics.CheckSphere(groundCheck.position, 0.2f, Ground);
         isMagnetic = Physics.CheckSphere(groundCheck.position, 0.2f, Magnetic);
         MyInput();
@@ -90,10 +105,36 @@ public class Movement : MonoBehaviour
             
     }
 
+
+
     private void FixedUpdate()
     {
-        MovePlayer();
+        moveDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        if(OnSlope() && !exitingSlope)
+        {
+            RB.AddForce(GetSlopeMoveDirection() * moveSpeed * 20f, ForceMode.Force);
+
+            if (RB.velocity.y > 0)
+            {
+                RB.AddForce(Vector3.down * 80f, ForceMode.Force);
+            }
+        }
+        else if(isMagnetic)
+        {
+            RB.AddForce(PlayerRotationMagnetic() * moveSpeed * 10f, ForceMode.Force);
+        }
+        else if(isGrounded)
+            RB.AddForce(moveDir.normalized * moveSpeed * 10f, ForceMode.Force);
+        else if(!isGrounded)
+            RB.AddForce(moveDir.normalized * moveSpeed * 7f * airMultiplier, ForceMode.Force);
+        if(OnSlope() || isMagnetic)
+        {
+            RB.useGravity = false;
+        }
+        else RB.useGravity = true;
     }
+
 
     private void StateHandler()
     {
@@ -121,37 +162,6 @@ public class Movement : MonoBehaviour
         }
     }
 
-    private void MovePlayer()
-    {
-        moveDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
-
-        if(OnSlope() && !exitingSlope)
-        {
-
-            RB.AddForce(GetSlopeMoveDirection() * moveSpeed * 20f, ForceMode.Force);
-
-            if (RB.velocity.y > 0)
-            {
-                RB.AddForce(Vector3.down * 80f, ForceMode.Force);
-            }
-        }
-
-        else if(isMagnetic)
-        {
-            RB.AddForce(PlayerRotationMagnetic() * moveSpeed * 10f, ForceMode.Force);
-        }
-
-        else if(isGrounded)
-            RB.AddForce(moveDir.normalized * moveSpeed * 10f, ForceMode.Force);
-        else if(!isGrounded)
-            RB.AddForce(moveDir.normalized * moveSpeed * 7f * airMultiplier, ForceMode.Force);
-
-        if(OnSlope() || isMagnetic)
-        {
-            RB.useGravity = false;
-        }
-        else RB.useGravity = true;
-    }
 
     private void SpeedLimiter()
     {
@@ -181,12 +191,15 @@ public class Movement : MonoBehaviour
         RB.velocity = new Vector3(RB.velocity.x, jumpForce, RB.velocity.z);
     }
 
+
     private void ResetJump()
     {
         readyToJump = true;
         exitingSlope = false;
     }
 
+
+    //NB in order to check the distance from the ground we use one ray that starts from the center of the player and goes down a bit more of its half height
     public bool OnSlope()
     {
         if(Physics.Raycast(groundCheck.position, Vector3.down, out slopeHit, 0.3f, Ground))
@@ -196,6 +209,7 @@ public class Movement : MonoBehaviour
         }
         return false;
     }
+
 
     public bool OnRamp()
     {
@@ -207,10 +221,12 @@ public class Movement : MonoBehaviour
         return false;
     }
 
+
     public Vector3 GetSlopeMoveDirection()
     {
         return Vector3.ProjectOnPlane(moveDir, slopeHit.normal).normalized;
     }
+
 
     Vector3 PlayerRotationMagnetic()
     {
