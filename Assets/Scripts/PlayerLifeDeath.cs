de5617274f591aae079cc152631259bc9f2d5995
Agnese <s305900@studenti.polito.it>
2023-05-31 18:42:38 +0200

    ora se muore dovrebbe tornare al checkpoint (paradiso)

--- Assets/Scripts/PlayerLifeDeath.cs
+++ Assets/Scripts/PlayerLifeDeath.cs
@@ -1,33 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerLifeDeath : MonoBehaviour
 {
-    private int _lives = 100;
-    public int Lives { get => _lives; } //takes the value of _lives and makes it a public variable
-    [SerializeField] private Vector3 _respawnPoint;
+    [SerializeField] private Animator _playerAnimator;
+
+    [Space]
+    [Range(0, 100f)] public float playerHealth;
+    public float maxPlayerHealth = 100f;
+    public Image[] HealthBarIMG;
+
+    [Space]
+    public float lightDamage = 10;
+    public float mediumDamage = 20;
+    public float bigDamage = 30;
+
+    private Vector3 _respawnPoint;
     void Start()
     {
-        
+        playerHealth = maxPlayerHealth;
+        _respawnPoint = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        DeadZone.OnPlayerFall += Fall; //to die after falling
+        if(transform.position.y < -10f)
+        {
+           transform.position = _respawnPoint;
+            playerHealth = maxPlayerHealth;
+        }
+        HealthBarFiller();
     }
 
-    private void Fall()
+
+    public void lightAttack()
     {
-        _lives -= _lives;
+        playerHealth -= lightDamage;
+    }
 
-        if(_lives == 0)
+    public void mediumAttack()
+    {
+        playerHealth -= mediumDamage;
+    }
+
+    public void bigAttack()
+    {
+        playerHealth -= bigDamage;
+    }
+
+
+    private void HealthBarFiller()
+    {
+        for(int i=0; i < HealthBarIMG.Length; i++)
         {
-            SceneManager.LoadScene(0);
+            if (playerHealth <= maxPlayerHealth && playerHealth > maxPlayerHealth / HealthBarIMG.Length * i)
+                HealthBarIMG[i].enabled = true;
+            else 
+                HealthBarIMG[i].enabled = false;
         }
+    }
 
-        transform.position = _respawnPoint;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Checkpoint"))
+        {
+            _respawnPoint = other.transform.position;
+            Destroy(other.gameObject);
+            Debug.Log("checkpoint");
+        }
     }
 }
