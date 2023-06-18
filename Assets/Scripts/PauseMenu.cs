dbe9268176fc9b5f0d7e9497d6cf24a1bb78bfd7
Agnese <s305900@studenti.polito.it>
2023-06-18 10:43:05 +0200

    okay piccola modifichina

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -29,6 +29,7 @@ public class PauseMenu : MonoBehaviour
         PauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
+        Cursor.visible = false;
     }
 
     void Pause()
