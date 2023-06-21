bf454a0891fe4b6cb08fbff75d71bd6bc3c719da
Agnese <s305900@studenti.polito.it>
2023-06-21 23:26:21 +0200

    saltini saltini ma nella scena e con uno script

---
+++ Assets/Scripts/DisappearingPlatform.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisappearingPlatform : MonoBehaviour
+{
+    public float disappearingTime;
+    [SerializeField] private GameObject[] _platforms;
+    //public float appearingTime;
+
+    public float currentTime;
+    private bool isEnabled;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isEnabled = true;
+        currentTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        if(currentTime >= disappearingTime)
+        {
+            currentTime = 0;
+            Hide();
+        }else if(currentTime >= (disappearingTime * 1/3) && currentTime < (disappearingTime * 2 / 3) && isEnabled)
+        {
+            _platforms[0].SetActive(false);
+            _platforms[1].SetActive(true);
+        } else if (currentTime >= (disappearingTime * 2 / 3) && isEnabled)
+        {
+            _platforms[1].SetActive(false);
+            _platforms[2].SetActive(true);
+            
+        }
+
+        
+    }
+
+    void Hide() 
+    {
+        isEnabled = !isEnabled;
+        if (isEnabled)
+        {
+            _platforms[0].SetActive(true);
+        } else
+        {
+            _platforms[2].SetActive(false);
+        }
+     
+    }
+}
