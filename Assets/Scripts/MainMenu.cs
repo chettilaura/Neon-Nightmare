ccae832cb2839a65e3beac2810051757f91e78aa
Agnese <s305900@studenti.polito.it>
2023-07-06 20:41:14 +0200

    volume e fullscreen

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private Slider _volume;
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -15,5 +17,18 @@ public class MainMenu : MonoBehaviour
     {
         Debug.Log("QUIT :(");
         Application.Quit();
+    }
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = _volume.value;
+        Debug.Log("volume :" + _volume.value);
+    }
+
+    public void FullScreen()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+        Debug.Log(Screen.fullScreen);
+
     }
 }
