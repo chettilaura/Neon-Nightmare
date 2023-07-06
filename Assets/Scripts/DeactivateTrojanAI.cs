8e7e116d45bb844314e2692fd681c62ac36f1772
Agnese <s305900@studenti.polito.it>
2023-07-06 16:58:22 +0200

    virus attaccano solo se si sentono minacciati

---
+++ Assets/Scripts/DeactivateTrojanAI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateTrojanAI : MonoBehaviour
+{
+    private Trojan _ai;
+    private float _minDist = 500;
+    [SerializeField] private GameObject _player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _ai = GetComponent<Trojan>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = (transform.position - _player.transform.position).sqrMagnitude;
+        if (dist > _minDist)
+            _ai.enabled = false;
+        else
+            _ai.enabled = true;
+    }
+}
