143b695b0340a68e918cc3ed30852c91fa13a7df
Agnese <s305900@studenti.polito.it>
2023-07-07 13:57:49 +0200

    sempre menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,6 +8,7 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private Slider _volume;
+    [SerializeField] private Toggle _notFullScreen;
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -27,8 +28,9 @@ public class MainMenu : MonoBehaviour
 
     public void FullScreen()
     {
-        Screen.fullScreen = !Screen.fullScreen;
-        Debug.Log(Screen.fullScreen);
-
+        if (_notFullScreen.isOn)
+            Screen.fullScreen = false;
+        else
+            Screen.fullScreen = true;
     }
 }
